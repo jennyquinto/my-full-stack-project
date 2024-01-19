e71a5219f0887285de781c5e5be94052ad668a25
jennyquinto <vanessaquinto719@gmail.com>
2024-01-19 18:57:01 -0500

    Setup eslintrc file

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    parser: "babel-eslint",
+    env: {
+      browser: true,
+      commonjs: true,
+      es6: true,
+      node: true,
+      jest: true,
+    },
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    plugins: ["react"],
+    rules: {
+        indent: ['error', 2, {SwitchCase: 1}], 
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'no-console': ['warn', {allow: ['clear', 'info', 'error', 'dir', 'trace']}],
+    },
+  };
