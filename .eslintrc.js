c393429701c903b03be0c1bc1a78c94c70d1feeb
jennyquinto <vanessaquinto719@gmail.com>
2024-01-19 19:09:28 -0500

    setup eslintrc file end

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,28 @@
 module.exports = {
     parser: "babel-eslint",
     env: {
-      browser: true,
-      commonjs: true,
-      es6: true,
-      node: true,
-      jest: true,
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
+        jest: true,
     },
     extends: ["eslint:recommended", "plugin:react/recommended"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        sourceType: "module",
+    },
     plugins: ["react"],
     rules: {
-        indent: ['error', 2, {SwitchCase: 1}], 
+        indent: ['error', 2, { SwitchCase: 1 }],
         'linebreak-style': ['error', 'unix'],
         quotes: ['error', 'single'],
         semi: ['error', 'always'],
-        'no-console': ['warn', {allow: ['clear', 'info', 'error', 'dir', 'trace']}],
-    },
-  };
+        'no-console': [
+            'warn',
+            { allow: ['clear', 'info', 'error', 'dir', 'trace'] }
+        ]
+    }
+};
