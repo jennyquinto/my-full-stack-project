878a08280bae48e5330dcb59ba31f3b3903062f0
jennyquinto <vanessaquinto719@gmail.com>
2024-01-19 19:43:46 -0500

    config eslintrc from jscomplete.com

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,33 @@
 module.exports = {
-    parser: "babel-eslint",
+    parser: "@babel/eslint-parser",
     env: {
-        browser: true,
-        commonjs: true,
-        es6: true,
-        node: true,
-        jest: true,
+      browser: true,
+      commonjs: true,
+      es6: true,
+      node: true,
+      jest: true,
     },
-    extends: ["eslint:recommended", "plugin:react/recommended"],
     parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        sourceType: "module",
+      ecmaVersion: 2020,
+      ecmaFeatures: {
+        impliedStrict: true,
+        jsx: true,
+      },
+      sourceType: "module",
     },
-    plugins: ["react"],
+    plugins: ["react", "react-hooks"],
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+    ],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
     rules: {
-        indent: ['error', 2, { SwitchCase: 1 }],
-        'linebreak-style': ['error', 'unix'],
-        quotes: ['error', 'single'],
-        semi: ['error', 'always'],
-        'no-console': [
-            'warn',
-            { allow: ['clear', 'info', 'error', 'dir', 'trace'] }
-        ]
-    }
-};
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  };
