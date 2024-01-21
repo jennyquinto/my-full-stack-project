2b112cae71b6c5e2aa80ed6ce59d467f67df4242
jennyquinto <vanessaquinto719@gmail.com>
2024-01-21 18:12:36 -0500

    Add first react component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import {createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.getElementById("root"))
+const randomNum = Math.random()
+const color = randomNum > 0.5 ? 'green' : 'red'
+
+const App = (props) => {
+    return (
+        <h1 className='text-center' style={{ color }}>
+            {props} -- {randomNum}
+        </h1>
+    )
+}
+
 
 root.render(
-    <h1>Hello Jen! from React</h1>
+    <App headerMessage='Hello props'/>
 )
