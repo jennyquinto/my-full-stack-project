c304797cbc77cb8ff640a9c904fa1c3d18a275e3
jennyquinto <vanessaquinto719@gmail.com>
2024-01-25 12:43:00 -0500

    display first contest properties

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
 import React from "react"
 
 import Header from "./Header"
+import ContestPreview from "./ContestPreview"
 
 class App extends React.Component {
     state = {
         pageHeader: "Naming Contests"
     }
-    componentDidMount(){
+    componentDidMount() {
         // timers, listeners, ajax requests
     }
-    componentWillUnmount(){
+    componentWillUnmount() {
         // clean timers liste
     }
     render() {
         return (
             <div className='App'>
                 <Header message={this.state.pageHeader} />
-                <div className="content">
-                    ...
+                <div>
+                    <ContestPreview {...this.props.contests[0]} />
                 </div>
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 
 import App from "./components/App"
 import data from "./data.json"
-console.log(data)
 
 const root = createRoot(document.getElementById('root'))
 
