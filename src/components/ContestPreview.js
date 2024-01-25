df70b106e630ac0f1a5f0baf8203de4d1aa8a7cb
jennyquinto <vanessaquinto719@gmail.com>
2024-01-25 13:12:53 -0500

    style contestPreview

--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const ContestPreview = (contest) => {
     return (
-        <div className='contestPreview'>
-            <div>
+        <div className='ContestPreview'>
+            <div className='category-name'>
                 {contest.categoryName}
             </div>
-            <div>
+            <div className='contest-name'>
                 {contest.contestName}
             </div>
         </div>
