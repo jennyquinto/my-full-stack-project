25aa7023591adb7990e8c5b07c465d7fb327adbd
jennyquinto <vanessaquinto719@gmail.com>
2024-01-24 22:53:35 -0500

    react component life cycle

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from "react"
 
 import Header from "./Header"
+import data from "../data.json"
+console.log(data)
 
 class App extends React.Component {
     state = {
         pageHeader: "Naming Contests"
+    }
+    componentDidMount(){
+        // timers, listeners, ajax requests
+    }
+    componentWillUnmount(){
+        // clean timers liste
     }
     render() {
         return (
