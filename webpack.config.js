6ade83f42c62db71824fdce036054a60bd239cd2
jennyquinto <vanessaquinto719@gmail.com>
2024-01-25 12:29:37 -0500

    resolve json loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,20 @@
 const path = require('path')
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-      path: path.resolve('public'),
-      filename: 'bundle.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.json$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "json-loader",
-          },
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve('public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-          },
-        },
-      ],
-    },
-  };
+      },
+    ],
+  },
+};
