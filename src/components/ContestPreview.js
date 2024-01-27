4ffc08d1c16d52860d66740a00b57aeab2ec528b
jennyquinto <vanessaquinto719@gmail.com>
2024-01-27 09:47:52 -0500

    dynamic onclick function for each contest

--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class ContestPreview extends Component {
+    handleClick = () => {
+        console.log(this.props.contestName)
+    }
     render() {
         return (
-            <div className='ContestPreview'>
+            <div className='ContestPreview' onClick={this.handleClick}>
                 <div className='category-name'>
                     {this.props.categoryName}
                 </div>
@@ -17,6 +20,6 @@ class ContestPreview extends Component {
 }
 ContestPreview.propTypes = {
     categoryName: PropTypes.string.isRequired,
-    constestName: PropTypes.string.isRequired,
+    contestName: PropTypes.string.isRequired,
 }
 export default ContestPreview
