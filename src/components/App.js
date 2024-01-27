f57b56ea60dfc5babcd0d2ef5d7c17694daec0cd
jennyquinto <vanessaquinto719@gmail.com>
2024-01-27 09:21:54 -0500

    add contestListo to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import Header from "./Header"
-import ContestPreview from "./ContestPreview"
+import ContestList from "./ContestList";
 
 class App extends React.Component {
     state = {
@@ -18,6 +18,7 @@ class App extends React.Component {
         return (
             <div className='App'>
                 <Header message={this.state.pageHeader} />
+                <ContestList contests={this.state.contests} />
             </div>
         );
     }
--- src/components/ContestList.js
+++ src/components/ContestList.js
@@ -3,15 +3,14 @@ import PropsType from 'prop-types'
 
 import ContestPreview from './ContestPreview'
 
-const ContestList = ({ contests }) => {
-    return (
-        <div className='ContestList'>
-            {contests.map(contest =>
-                <ContestPreview key={contest.id} {...contest} />
-            )}
-        </div>
-    )
-}
+const ContestList = ({ contests }) => (
+    <div className='ContestList'>
+        {contests.map(contest =>
+            <ContestPreview key={contest.id} {...contest} />
+        )}
+    </div>
+)
+
 ContestList.prototype = {
     contests: PropsType.array
 }
