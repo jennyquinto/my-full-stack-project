51d6fef65fc3610d3ee62bdd36f279c720f16c94
jennyquinto <vanessaquinto719@gmail.com>
2024-01-27 10:17:14 -0500

    build  router funct

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
-import React from "react"
+import React from 'react'
 
-import Header from "./Header"
-import ContestList from "./ContestList";
+import Header from './Header'
+import ContestList from './ContestList';
+
+const pushState = (obj, url) =>
+    window.history.pushState(obj, '', url)
+
 
 class App extends React.Component {
     state = {
