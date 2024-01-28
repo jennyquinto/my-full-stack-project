ee457fdf1bb425fb58c6c1949f410397ae6e8b60
jennyquinto <vanessaquinto719@gmail.com>
2024-01-28 12:48:06 -0500

    conver the data array to an data obj

--- api/index.js
+++ api/index.js
@@ -3,8 +3,13 @@ import data from "../src/data";
 
 const router = express.Router()
 
+const contests = data.contests.reduce((obj, contest, index) => {
+    obj[index + 1] = contest
+    return obj
+}, {})
+
 router.get('/contests', (req, res) => {
-    res.send({ contests: data.contests })
+    res.send({ contests: contests })
 })
 
 export default router
