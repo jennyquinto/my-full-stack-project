886ece9a5b7765dc3643a3f235df2ff73eaa9844
jennyquinto <vanessaquinto719@gmail.com>
2024-01-28 16:56:52 -0500

    fecht content inf from api -api/router

--- api/index.js
+++ api/index.js
@@ -3,13 +3,19 @@ import data from "../src/data";
 
 const router = express.Router()
 
-const contests = data.contests.reduce((obj, contest, index) => {
+const contests = data.contests.reduce((obj, contest) => {
     obj[contest.id] = contest
     return obj
 }, {})
 
 router.get('/contests', (req, res) => {
     res.send({ contests: contests })
+})
+
+router.get('/contests/:contestId', (req, res) => {
+    const contest = contests[req.params.contestId]
+    contest.description = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam magni, dolor eos corrupti perspiciatis nihil quibusdam, cumque dolorem suscipit totam consectetur aperiam saepe voluptatibus? Commodi itaque excepturi accusamus aut aliquam.'
+    res.send(contest)
 })
 
 export default router
